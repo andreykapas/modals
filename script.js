cbe7e475c3fb2018793d31cab88396808e2cabb7
Andrei <andreykapas@gmail.com>
2026-03-26 09:16:04 +0100

    add event lestners

--- script.js
+++ script.js
@@ -3,4 +3,21 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.open-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const showClickHandler = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeBtnHandler = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let index = 0; index < btnOpenModal.length; index++) {
+  btnOpenModal[index].addEventListener('click', showClickHandler);
+}
+
+btnCloseModal.addEventListener('click', closeBtnHandler);
+overlay.addEventListener('click', closeBtnHandler);
